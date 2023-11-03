af89f34444e04db7a77c9c9c0e3f31e2662ea74d
airmanxtw <airmanx@stust.edu.tw>
2023-11-03 11:42:11 +0800

    init

---
+++ NewProj.test/UnitTest1.cs
@@ -0,0 +1,24 @@
+using LanguageExt.UnsafeValueAccess;
+using NewProj.lib;
+namespace NewProj.test;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Method md = new();
+        var result = md.Div(0);
+        Assert.IsTrue(result.IsLeft);
+    }
+
+    [TestMethod]
+    public void TestMethod2()
+    {
+        Method md = new();
+        var result = md.Div(2);
+        Assert.AreEqual(0.5, result.Value());
+    }
+
+}
---
+++ NewProj/lib/method.cs
@@ -0,0 +1,21 @@
+using LanguageExt;
+using static LanguageExt.Prelude;
+namespace NewProj.lib;
+public class Method
+{
+    public string Hello()
+    {
+        return "hello";
+    }
+    public int Add(int a, int b)
+    {
+        return a + b + 2;
+    }
+
+    public Either<string, double> Div(double x)
+    {
+        if (x == 0)
+            return Left<string, double>("div by zero");
+        return Right<string, double>(1 / x);
+    }
+}
